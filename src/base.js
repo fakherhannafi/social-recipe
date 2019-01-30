a163a4d8215075357a7eb1cfdfc0c39db1818e38
fakhrovski <Fakhrovski@DESKTOP-LDMK5KF>
2019-01-30 19:35:04 +0100

    add firebase credentials & fix dependency bug

--- src/base.js
+++ src/base.js
@@ -3,9 +3,9 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'xxx',
-  authDomain: 'xxx',
-  databaseURL: 'xxx'
+  apiKey: 'AIzaSyAx36mC5d-ZY0m_SvO16wTbyesKov2B0KQ',
+  authDomain: 'recette-a7893.firebaseapp.com',
+  databaseURL: 'https://recette-a7893.firebaseio.com'
 })
 
 const base = Rebase.createClass(firebaseApp.database())
